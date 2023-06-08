9f97115d01de3f51bcc5e561e08ea05fe863e87b
Christian <contact@komodo.link>
2023-06-08 03:57:46 +0200

    move to a `src/` structure

--- src/aiopusher/__init__.py
+++ src/aiopusher/__init__.py
@@ -1,1 +1,5 @@
 """aiopusher is an async client for Pusher."""
+
+
+def __version__():
+    return "0.1.0"
--- tests/test_test.py
+++ tests/test_test.py
@@ -1,6 +1,13 @@
 """This is a test file for testing the testing framework."""
 
+from aiopusher import __version__
+
 
 def test_blah():
     """Test for testing purposes."""
     assert True
+
+
+def test_version():
+    """Test version."""
+    assert __version__() == "0.1.0"
