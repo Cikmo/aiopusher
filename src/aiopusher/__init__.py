0f4c953961f945053e2b3526e9ed91c5d6538382
Christian <contact@komodo.link>
2023-06-09 15:13:38 +0200

    remove test code

--- src/aiopusher/__init__.py
+++ src/aiopusher/__init__.py
@@ -1,27 +1,5 @@
 """aiopusher is an async client for Pusher."""
 
 
-from typing_extensions import Self
-
-
 def __version__():
     return "0.1.0"
-
-
-class Blah:
-    """Blah blah blah."""
-
-    def __init__(self, name: str):
-        self.name = name
-
-    def __repr__(self):
-        return f"Blah({self.name})"
-
-    def return_self(self) -> Self:
-        """Return self."""
-        return self
-
-
-one = Blah("one")
-
-two = one.return_self().return_self()
