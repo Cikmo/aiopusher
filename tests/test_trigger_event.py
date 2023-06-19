a10132058179475181f6e360600be80f9ff25a22
Christian <contact@komodo.link>
2023-06-19 20:01:17 +0200

    add version to test pusher message

--- tests/test_trigger_event.py
+++ tests/test_trigger_event.py
@@ -1,5 +1,7 @@
 """Test trigger event to pusher channel"""
 
+import sys
+
 from .pusher_trigger_event import trigger_event
 
 
@@ -7,5 +9,5 @@ def test_trigger_event():
     """Test trigger event to pusher channel"""
     channel = "my-channel"
     event = "my-event"
-    data = {"message": "hello world"}
+    data = {"message": f"hello world, from {sys.version}"}
     trigger_event(channel, event, data)
